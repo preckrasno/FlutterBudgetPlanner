6742b29d5b3d5591c66691142b3a434a7cb905ba
Dmytro Muntian <dev@nazh.me>
2021-01-18 21:34:14 +0200

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_budget_planner/models/user.dart';
 import 'package:flutter_budget_planner/screens/wrapper.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_budget_planner/services/auth.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,8 +15,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: WrapperWidgetClass(),
+    return StreamProvider<UserClass>.value(
+      value: AuthServiceClass().user,
+      child: MaterialApp(
+        home: WrapperWidgetClass(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_budget_planner/models/user.dart';
 import 'package:flutter_budget_planner/screens/authenticate/authenticate.dart';
 import 'package:flutter_budget_planner/screens/home/home.dart';
+import 'package:provider/provider.dart';
 
 class WrapperWidgetClass extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user = Provider.of<UserClass>(context);
+    print('WrapperWidgetClass user = $user');
+
     //return either Home or Authenticate widget
     return AuthenticateWidgetClass();
   }
