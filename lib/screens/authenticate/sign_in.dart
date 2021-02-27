1549e07d783d878e6ed274261cac88c212565f61
Dmytro Muntian <dev@nazh.me>
2021-02-27 23:50:30 +0200

    refactor(sing files): tabulation

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -23,84 +23,87 @@ class _SignInWidgetClassState extends State<SignInWidgetClass> {
 
   @override
   Widget build(BuildContext context) {
-    return isLoading ? LoadingWidget() : Scaffold(
-      appBar: AppBar(
-        title: Text('Sign in to Budget Planner'),
-        actions: <Widget>[
-          FlatButton.icon(
-            onPressed: () {
-              widget.toggleViewParam();
-            },
-            icon: Icon(Icons.person),
-            label: Text('Sign Up'),
-          )
-        ],
-      ),
-      body: Container(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Email',
+    return isLoading
+        ? LoadingWidget()
+        : Scaffold(
+            appBar: AppBar(
+              title: Text('Sign in to Budget Planner'),
+              actions: <Widget>[
+                FlatButton.icon(
+                  onPressed: () {
+                    widget.toggleViewParam();
+                  },
+                  icon: Icon(Icons.person),
+                  label: Text('Sign Up'),
+                )
+              ],
+            ),
+            body: Container(
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Email',
+                        hintText: 'Email',
+                      ),
+                      keyboardType: TextInputType.emailAddress,
+                      autofillHints: [AutofillHints.email],
+                      validator: (val) => val.isEmpty ? 'Enter an email' : null,
+                      onChanged: (val) {
+                        setState(() => email = val);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      decoration: InputDecoration(
+                        labelText: 'Password',
+                        hintText: 'Password',
+                      ),
+                      enableSuggestions: false,
+                      autofillHints: [AutofillHints.password],
+                      autocorrect: false,
+                      obscureText: true,
+                      validator: (val) => val.length < 6
+                          ? 'Enter a password 6+ chars long'
+                          : null,
+                      onChanged: (val) {
+                        setState(() => password = val);
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    RaisedButton(
+                      child: Text('Sign in'),
+                      onPressed: () async {
+                        if (_formKey.currentState.validate()) {
+                          setState(() {
+                            isLoading = true;
+                          });
+                          print('sign_in.dart valid');
+                          dynamic result = await _auth.signInWithEmailPassword(
+                              email, password);
+                          if (result == null) {
+                            setState(() {
+                              error = 'Could not sign in';
+                              isLoading = false;
+                            });
+                          }
+                        }
+                      },
+                    ),
+                    Text(error),
+                  ],
                 ),
-                keyboardType: TextInputType.emailAddress,
-                autofillHints: [AutofillHints.email],
-                validator: (val) => val.isEmpty ? 'Enter an email' : null,
-                onChanged: (val) {
-                  setState(() => email = val);
-                },
               ),
-              SizedBox(
-                height: 20.0,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Password',
-                ),
-                enableSuggestions: false,
-                autofillHints: [AutofillHints.password],
-                autocorrect: false,
-                obscureText: true,
-                validator: (val) =>
-                    val.length < 6 ? 'Enter a password 6+ chars long' : null,
-                onChanged: (val) {
-                  setState(() => password = val);
-                },
-              ),
-              SizedBox(
-                height: 20.0,
-              ),
-              RaisedButton(
-                child: Text('Sign in'),
-                onPressed: () async {
-                  if (_formKey.currentState.validate()) {
-                    setState(() {
-                      isLoading = true;
-                    });
-                    print('sign_in.dart valid');
-                    dynamic result =
-                        await _auth.signInWithEmailPassword(email, password);
-                    if (result == null) {
-                      setState(() {
-                        error = 'Could not sign in';
-                        isLoading = false;
-                      });
-                    }
-                  }
-                },
-              ),
-              Text(error),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          );
   }
 }
